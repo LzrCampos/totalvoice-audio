af83ddf87d65a8613c3cfc8e487a7dde6e0320a2
LzrCampos <lazarocampos98@gmail.com>
2019-01-18 07:52:32 -0200

    init

---
+++ bin/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const totalvoice = require('totalvoice-node');
+const client = new totalvoice("4cf42ca5af35a1727938151bb2e11c1e");
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+gemidao = (to) => {
+  client.audio.enviar(to, "https://github.com/haskellcamargo/gemidao-do-zap/raw/master/resources/gemidao.mp3", false, "11995781688", false)
+    .then(function (data) {
+      console.log(data)
+    })
+    .catch(function (error) {
+      console.error('Erro: ', error)
+    });
+}
+
+const server = http.createServer((req, res) => {
+  gemidao(req.url);
+  res.statusCode = 200;
+});
+
+server.listen(port, () => {
+  console.log(`Server running at PORT 3000`);
+});
